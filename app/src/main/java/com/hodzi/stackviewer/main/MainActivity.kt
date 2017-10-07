dd9cea1146507cacd0e72409cc4e2e86c89a990b
Valeriy Makarshin <valery.makarshin@flatstack.com>
2017-10-07 18:21:08 +0300

    Added kotlin-android-extensions

--- app/src/main/java/com/hodzi/stackviewer/main/MainActivity.kt
+++ app/src/main/java/com/hodzi/stackviewer/main/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.hodzi.stackviewer.main
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.view.Menu
 import android.view.MenuItem
 import com.hodzi.stackviewer.R
 import com.hodzi.stackviewer.di.Injector
 import com.hodzi.stackviewer.utils.base.BaseActivity
 import com.hodzi.stackviewer.utils.ui.ActivityInfo
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.view_toolbar.*
 
-class MainActivity : BaseActivity<MainView, MainPresenter>(), MainView {
-    override fun getActivityInfo(): ActivityInfo = ActivityInfo(R.layout.activity_main)
+class MainActivity : BaseActivity<MainView, MainPresenter>(),
+    MainView, NavigationView.OnNavigationItemSelectedListener {
 
+    override fun getActivityInfo(): ActivityInfo =
+        ActivityInfo(R.layout.activity_main, uiToolbar, R.string.app_name)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Injector.inject(this)
         super.onCreate(savedInstanceState)
+
+        val toggle = ActionBarDrawerToggle(this, uiMainDl, uiToolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        uiMainDl.setDrawerListener(toggle)
+        toggle.syncState()
+
+        uiMainNv.setNavigationItemSelectedListener(this)
     }
 
     override fun parseArguments(extras: Bundle) {
@@ -37,4 +51,35 @@ class MainActivity : BaseActivity<MainView, MainPresenter>(), MainView {
 
     }
 
+    override fun onBackPressed() {
+        if (uiMainDl.isDrawerOpen(GravityCompat.START)) {
+            uiMainDl.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        val id = item.itemId
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        uiMainDl.closeDrawer(GravityCompat.START)
+        return true
+    }
 }
