3222213457fb0ec2c1e03b91142eeaf40b6ab8d3
Valeriy Makarshin <valery.makarshin@flatstack.com>
2017-10-17 22:56:36 +0300

    Added field in api from model

--- app/src/main/java/com/hodzi/stackviewer/utils/Api.kt
+++ app/src/main/java/com/hodzi/stackviewer/utils/Api.kt
@@ -1,19 +1,42 @@
 package com.hodzi.stackviewer.utils
 
-import com.hodzi.stackviewer.model.Block
-import com.hodzi.stackviewer.model.Question
-import com.hodzi.stackviewer.model.Tag
-import com.hodzi.stackviewer.model.User
+import com.hodzi.stackviewer.model.*
 import io.reactivex.Observable
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface Api {
-    @GET("/2.2/questions?order=desc&sort=activity&site=stackoverflow")
-    fun getQuestions(): Observable<Block<Question>>
+    @GET("/2.2/questions")
+    fun getQuestions(
+        @Query("order") order: String = Orders.DESC.value,
+        @Query("site") site: String = Sites.STACKOVERFLOW.value,
+        @Query("sort") sort: String = Sorts.ACTIVITY.value,
+        @Query("filter") filter: String = Filters.WITHBODY.value
+    ): Observable<Block<Question>>
 
-    @GET("/2.2/tags?site=stackoverflow&order=desc&sort=popular&filter=default")
-    fun getTags(): Observable<Block<Tag>>
+    @GET("/2.2/questions/questionId/answers")
+    fun getAnswers(
+        @Path("questionId") questionId : Int,
+        @Query("order") order: String = Orders.DESC.value,
+        @Query("site") site: String = Sites.STACKOVERFLOW.value,
+        @Query("sort") sort: String = Sorts.ACTIVITY.value,
+        @Query("filter") filter: String = Filters.WITHBODY.value
+    ): Observable<Block<Answer>>
 
-    @GET("/2.2/users?site=stackoverflow&order=desc&sort=reputation&filter=default")
-    fun getUsers(): Observable<Block<User>>
+    @GET("/2.2/tags")
+    fun getTags(
+        @Query("order") order: String = Orders.DESC.value,
+        @Query("site") site: String = Sites.STACKOVERFLOW.value,
+        @Query("sort") sort: String = Sorts.POPULAR.value,
+        @Query("filter") filter: String = Filters.WITHBODY.value
+    ): Observable<Block<Tag>>
+
+    @GET("/2.2/users")
+    fun getUsers(
+        @Query("order") order: String = Orders.DESC.value,
+        @Query("site") site: String = Sites.STACKOVERFLOW.value,
+        @Query("filter") filter: String = Filters.WITHBODY.value,
+        @Query("sort") sort: String = Sorts.REPUTATION.value
+    ): Observable<Block<User>>
 }
