7176865b0f567301791dd5025b91ef4540306247
Valeriy Makarshin <valery.makarshin@flatstack.com>
2017-11-09 19:42:10 +0300

    Fixed mock rx

---
+++ app/src/test/java/com/hodzi/stackviewer/RxHook.kt
@@ -0,0 +1,19 @@
+package com.hodzi.stackviewer
+
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+
+class RxHook {
+    companion object {
+        fun initSchedulers() {
+            val immediate = Schedulers.trampoline()
+            RxJavaPlugins.setInitIoSchedulerHandler { immediate }
+            RxJavaPlugins.setInitComputationSchedulerHandler { immediate }
+            RxJavaPlugins.setInitNewThreadSchedulerHandler { immediate }
+            RxJavaPlugins.setInitSingleSchedulerHandler { immediate }
+            RxAndroidPlugins.setInitMainThreadSchedulerHandler { immediate }
+            RxJavaPlugins.reset()
+        }
+    }
+}
