0aee0516b8ff7b4a5652653b4ff5217f6baf95d3
Valeriy Makarshin <valery.makarshin@flatstack.com>
2018-04-10 19:59:17 +0300

    Removed view

--- app/src/main/java/com/hodzi/stackviewer/login/LoginView.kt
+++
@@ -1,3 +0,0 @@
-package com.hodzi.stackviewer.login
-
-interface LoginView
--- app/src/main/java/com/hodzi/stackviewer/main/di/MainModule.kt
+++ app/src/main/java/com/hodzi/stackviewer/main/di/MainModule.kt
@@ -2,6 +2,7 @@ package com.hodzi.stackviewer.main.di
 
 import com.hodzi.stackviewer.StackScope
 import com.hodzi.stackviewer.di.AccountManager
+import com.hodzi.stackviewer.main.MainContract
 import com.hodzi.stackviewer.main.MainPresenter
 import dagger.Module
 import dagger.Provides
@@ -9,7 +10,7 @@ import dagger.Provides
 @Module
 class MainModule {
     @Provides @StackScope
-    fun provideMainPresenter(accountManager: AccountManager): MainPresenter =
+    fun provideMainPresenter(accountManager: AccountManager): MainContract.Presenter =
         MainPresenter(accountManager)
 
     @Provides @StackScope
